c8f7fd6e28c82674bf8ad99b564dfbfbd9c97262
kate <katerina.savras@gmail.com>
2023-04-21 14:45:49 +0200

    add home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="container">
+            <h1 className="app__title"> Frontend <br/> Interview Preparation</h1>
+            <nav className="icons">
+                <ul>
+                    <li><span>HTML</span></li>
+                    <li><span>CSS</span></li>
+                    <li><span>JS</span></li>
+                </ul>
+            </nav>
+        </div>
     </div>
   );
 }
